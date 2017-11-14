33fc83a472be66608b3437d402f3f9ebf75d0757
vadimaz <vzorenko@gmail.com>
2017-11-14 09:36:14 +0200

    1

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Serializable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+    }
+    public AmigoSet(Collection<? extends E> collection){
+        this.map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        this.addAll(collection);
+
+    }
+
+    public boolean add(E e){
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.keySet().contains(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.keySet().remove(o);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+}
